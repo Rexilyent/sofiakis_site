45c87ad60d02d1680a8c922329ccaadb6e4733fd
Rexilyent <rexilyent@gmail.com>
2026-02-14 09:27:17 -0600

    Added function to create release_id

--- backend/tools/common/time_utils.py
+++ backend/tools/common/time_utils.py
@@ -7,3 +7,6 @@ from datetime import datetime, UTC
 
 def now_iso() -> str:
     return datetime.now(UTC).isoformat() + "Z"
+
+def release_id() -> str:
+    return datetime.now(UTC).strftime("%Y-%m-%dT%H-%M-%SZ")
